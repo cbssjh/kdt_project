f4db3a7fb4eb7b1b3c37d52266eea2ff5366a4dd
unknown <cbssjh@gmail.com>
2024-10-14 20:20:09 +0900

    CORS, 프록시, Express 서버 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import Login from "./Login";
+import './App.css'
+import React, { useEffect} from "react";
+import Login from "./components/Login";
 
 function App() {
-  return (
-    <div>
-      <Login />
-    </div>
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api")
+            .then(res => res.json())
+            .then(data => console.log(data))
+    });
+
+    return (
+        <div className={"App"}>
+            Test
+            <Login/>
+        </div>
   );
 }
 
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+    return (
+        <div>
+            <h2>Main Page</h2>
+            <p>Welcome to the main page!</p>
+        </div>
+    );
+}
+
+export default Main;
